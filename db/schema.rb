ac114fae7977a24a8ea0f425c9a55cb033785495
Von Christian Halip <vc.halip@gmail.com>
2021-07-30 20:51:26 +0800

    Add person model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_103457) do
+ActiveRecord::Schema.define(version: 2021_07_30_124949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -106,6 +106,14 @@ ActiveRecord::Schema.define(version: 2021_07_30_103457) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["code"], name: "index_liabilities_on_code"
     t.index ["institution_type", "institution_id"], name: "index_liabilities_on_institution"
+  end
+
+  create_table "people", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "first_name"
+    t.string "middle_name"
+    t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "revenues", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
---
+++ spec/factories/people.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :person do
+    first_name { "MyString" }
+    middle_name { "MyString" }
+    last_name { "MyString" }
+  end
+end
