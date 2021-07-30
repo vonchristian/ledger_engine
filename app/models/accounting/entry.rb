d33a2575f725c96315aee57043a9fe999643a9d8
Von Christian Halip <vc.halip@gmail.com>
2021-07-30 20:06:15 +0800

    Fix wrong class name association

--- app/models/accounting/entry.rb
+++ app/models/accounting/entry.rb
@@ -6,7 +6,7 @@ module Accounting
 
     belongs_to :institution, polymorphic: true
     has_many :debit_amounts, class_name: 'Accounting::Amounts::DebitAmount'
-    has_many :credit_amounts, class_name: 'Accounting::Amounts::DebitAmount'
+    has_many :credit_amounts, class_name: 'Accounting::Amounts::CreditAmount'
 
     validates :entry_date, :entry_time, :reference_number, :description, :recording_type, presence: true
   end
